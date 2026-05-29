2165f1a6bc0a7e6535c288d809c913c510727095
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-05-29 22:41:46 +0530

    solved some bugs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+
 let app=express()
+
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
+
 app.use(express.json({limit:"16kb"}))
+
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
+
 app.use(express.static("public"))
+
 app.use(cookieParser())
 
 import healthcheckrouter from "./routes/health.routes.js"
+
 app.use("/api/v1/healthcheck",healthcheckrouter)    
+
 export{app}
 
 
