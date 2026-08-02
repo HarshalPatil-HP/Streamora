b87286f1b68ec1691adbf996766f40a81b75714b
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-08-02 22:26:26 +0530

    Fix formatting of comment routes

--- backend/src/routes/comment.routes.js
+++ backend/src/routes/comment.routes.js
@@ -9,9 +9,9 @@ import { authmiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/:videoId").get(getVideoComments)
-router.route("/:videoId").post(authmiddleware, addComment)
-router.route("/c/:commentId").patch(authmiddleware, updateComment)
-router.route("/c/:commentId").delete(authmiddleware, deleteComment)
+router.route("/:videoId").get(getVideoComments);
+router.route("/:videoId").post(authmiddleware, addComment);
+router.route("/c/:commentId").patch(authmiddleware, updateComment);
+router.route("/c/:commentId").delete(authmiddleware, deleteComment);
 
 export default router
